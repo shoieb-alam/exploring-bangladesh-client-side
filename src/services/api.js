cd128fb11273f2d7dbf8f783c633ffef3b6da4f5
Shoieb Alam <shoieb.ctg@gmail.com>
2024-01-27 00:53:00 +0600

    add API url file

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+const API_BASE_URL = "https://tourism-website-server-site.vercel.app";
+
+// export const API_ENDPOINTS = {
+//     houses: `${API_BASE_URL}/houses`,
+//     reviews: `${API_BASE_URL}/reviews`,
+//     users: `${API_BASE_URL}/users`,
+//     bookings: `${API_BASE_URL}/bookings`,
+//     myApartments: `${API_BASE_URL}/myApartments`,
+// };
+
+export const servicesAPI = `${API_BASE_URL}/services`;
+export const bookingsAPI = `${API_BASE_URL}/bookings`;
+export const myPackagesAPI = `${API_BASE_URL}/myPackages`;
